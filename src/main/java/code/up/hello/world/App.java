8bde954964877068738ba286b49b3345d20b9c07
code-up <code.up.poo@gmail.com>
2014-01-30 22:19:53 +0000

    Sum Is 3 + 6

--- src/main/java/code/up/hello/world/App.java
+++ src/main/java/code/up/hello/world/App.java
@@ -4,10 +4,22 @@ package code.up.hello.world;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App  {
+    private static String Panda = "Cutie";
+    
+    private static int One = 1;
+    private static int Two = 2;
+    
+    public static void main( String[] args ) {
+        int sum = add(One, Two);
         System.out.println( "Hello World!" );
+        System.out.println( "Sum is "+sum );
+        int newNumber = App.add(sum, sum);
+        System.out.println( "Sum is "+newNumber);
+    }
+    
+    public static int add(int x, int y){
+        return x + y;
+        
     }
 }
