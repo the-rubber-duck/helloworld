1648dce79375f5e9eb2da0f9f721cbe1e10e0495
code-up <code.up.poo@gmail.com>
2014-02-04 14:24:50 +0000

    Subtract Working

--- src/main/java/code/up/hello/world/App.java
+++ src/main/java/code/up/hello/world/App.java
@@ -16,10 +16,14 @@ public class App  {
         System.out.println( "Sum is "+sum );
         int newNumber = App.add(sum, sum);
         System.out.println( "Sum is "+newNumber);
-    }
-    
+        int LawNumber = sub(One, Two);
+        System.out.println ("1 - 2 = "+LawNumber);
+        int LawNewNumber = App.sub(newNumber, sum);
+        System.out.println ("6 - 3 = " +LawNewNumber);}
+            
     public static int add(int x, int y){
-        return x + y;
+        return x + y;}
         
-    }
-}
+public static int sub(int x, int y){
+    return x - y;
+}} 
