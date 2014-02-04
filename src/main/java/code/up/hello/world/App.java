105eff20f5529fd2832f331dc040d3a26cc4f95e
code-up <code.up.poo@gmail.com>
2014-02-04 22:02:16 +0000

    Formatting...

--- src/main/java/code/up/hello/world/App.java
+++ src/main/java/code/up/hello/world/App.java
@@ -4,26 +4,30 @@ package code.up.hello.world;
  * Hello world!
  *
  */
-public class App  {
+public class App {
+
     private static String Panda = "Cutie";
-    
+
     private static int One = 1;
     private static int Two = 2;
-    
-    public static void main( String[] args ) {
+
+    public static void main(String[] args) {
         int sum = add(One, Two);
-        System.out.println( "Hello World!" );
-        System.out.println( "Sum is "+sum );
+        System.out.println("Hello World!");
+        System.out.println("Sum is " + sum);
         int newNumber = App.add(sum, sum);
-        System.out.println( "Sum is "+newNumber);
+        System.out.println("Sum is " + newNumber);
         int LawNumber = sub(One, Two);
-        System.out.println ("1 - 2 = "+LawNumber);
+        System.out.println("1 - 2 = " + LawNumber);
         int LawNewNumber = App.sub(newNumber, sum);
-        System.out.println ("6 - 3 = " +LawNewNumber);}
-            
-    public static int add(int x, int y){
-        return x + y;}
-        
-public static int sub(int x, int y){
-    return x - y;
-}} 
+        System.out.println("6 - 3 = " + LawNewNumber);
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+}
